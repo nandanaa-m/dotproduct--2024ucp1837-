87cf88040051fe80a676d3b0f84dc69e3f3d30f5
Tanmay <2024UCP1489@mnit.ac.in>
2025-11-17 15:42:53 +0530

    Nandana's changes to mahek's code

--- vecadd.py
+++ vecadd.py
@@ -3,3 +3,5 @@ from concurrent.futures import ThreadPoolExecutor
 def vecadd(a, b):
     with ThreadPoolExecutor() as executor:
         return list(executor.map(lambda x, y: x + y, a, b))
+        
+#nice code but i didnt understand it 
